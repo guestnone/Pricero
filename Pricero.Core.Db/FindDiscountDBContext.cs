a133bad84e0346933500460aa87add33405ec841
khardimonq <quarhodronofjarkendar@gmail.com>
2019-12-28 05:24:43 +0100

    Adding DBContext

---
+++ Pricero.Core.Db/FindDiscountDBContext.cs
@@ -0,0 +1,32 @@
+﻿using FindDiscount.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FindDiscount.DB
+{
+    public class FindDiscountDBContext : DbContext
+    {
+        public FindDiscountDBContext() : base("name=FindDiscountDB") {
+
+        }
+        
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<FavouriteShop> FavouriteShops { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<FavouriteProduct> FavouriteProducts { get; set; }
+        public DbSet<Chain> Chains { get; set; }
+        public DbSet<ChainProduct> ChainProducts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<ProductGroup> ProductGroups { get; set; }
+        public DbSet<PriceReport> PriceReports { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Thread> Threads { get; set; }
+        public DbSet<UserPost> UserPosts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
